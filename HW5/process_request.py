bd5d6f7d0937048424794f5ada29031eb9b1d23b
toshikistan <152557601+toshikistan@users.noreply.github.com>
2024-03-18 02:04:05 +0200

    Delete HW5 directory from wrong branch

--- HW5/process_request.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-def execute_query(query, args=()):
-    with sqlite3.connect('chinook.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute(query, args)
-        result = cursor.fetchall()
-    return result
