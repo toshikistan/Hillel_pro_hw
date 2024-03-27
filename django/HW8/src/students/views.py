a39ffcb19315b75d63618ea11a5350782d507170
toshikistan <152557601+toshikistan@users.noreply.github.com>
2024-03-27 11:33:06 +0200

    Delete django/HW8 directory

--- django/HW8/src/students/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def hello_world(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello World!")
